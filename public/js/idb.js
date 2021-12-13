c28fc8efa0484d30836348f65b002efbdd8545d6
Kamila Raggi <kamilaraggi@hotmail.com>
2021-12-12 20:40:35 -0500

    getting help for service-worker.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,15 +2,13 @@ let db;
 const request = indexedDB.open('budget', 1);
 
 request.onupgradeneeded = function (event) {
-    const db = event.target.result;
-    db.createObjectStore('new_budget', { autoIncrement: true });
+ const db = event.target.result;
+  db.createObjectStore('new_budget', { autoIncrement: true });
 };
-
 request.onsuccess = function (event) {
-    db = event.target.result;
-
-    if (navigator.onLine) {
-        uploadBudget();
+  db = event.target.result;
+ if (navigator.onLine) {
+      uploadBudget();
     }
 };
 
@@ -31,34 +29,34 @@ function uploadBudget() {
     const getAll = budgetObjectStore.getAll();
 
     getAll.onsuccess = function () {
-        if (getAll.result.length > 0) {
-            fetch('/api/transaction/bulk', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                }
+     if (getAll.result.length > 0) {
+    fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+      headers: {
+          Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+    }
             })
     //
-                .then(response => response.json())
-                .then(serverResponse => {
-                    //   console.log(serverResponse);
-                    if (serverResponse.message) {
-                        throw new Error(serverResponse);
-                     //
-                    }
-                    const transaction = db.transaction(['new_budget'], 'readwrite');
-                    const budgetObjectStore = transaction.objectStore('new_budget');
-                    budgetObjectStore.clear();
+    .then(response => response.json())
+   .then(serverResponse => {
+        //   console.log(serverResponse); ?
+      if (serverResponse.message) {
+      throw new Error(serverResponse);
+         //
+    }
+       const transaction = db.transaction(['new_budget'], 'readwrite');
+       const budgetObjectStore = transaction.objectStore('new_budget');
+       budgetObjectStore.clear();
 
-                    alert('All saved budgets have been submitted!');
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        }
-    };
+   alert('All saved budgets have been submitted!');
+   })
+    catch(err => {
+       console.log(err);
+   });
+ }
+};
 }
 
 window.addEventListener('online', uploadBudget);
